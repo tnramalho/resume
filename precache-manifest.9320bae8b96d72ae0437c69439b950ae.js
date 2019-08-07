d0e7a7aa8e9aa517cf5cef132189f8fe9890360b
Filipe Macedo <fmacedoo@MacBook-Pro-de-user.local>
2019-08-07 17:32:19 -0300

    Updates

---
+++ precache-manifest.9320bae8b96d72ae0437c69439b950ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6390637aa2c42473806edfd563c9d10",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "22464239592b0745a48d",
+    "url": "/resume/static/css/main.19c49ce9.chunk.css"
+  },
+  {
+    "revision": "802f4d008b3e4e8abb94",
+    "url": "/resume/static/js/2.0aaa8ac0.chunk.js"
+  },
+  {
+    "revision": "22464239592b0745a48d",
+    "url": "/resume/static/js/main.c8ac94b2.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
